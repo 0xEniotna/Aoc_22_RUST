489080e0359f12f232cba453339ded07861cd480
0xKarasu <0xKarasu@protonmail.com>
2022-12-06 13:44:46 +0100

    :rocket: add day6

---
+++ src/bin/day6.rs
@@ -0,0 +1,66 @@
+use eyre::Result;
+
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+fn main() -> Result<()> {
+    // question1()?;
+    question2()?;
+
+    Ok(())
+}
+
+fn question1() -> Result<()> {
+    let lines = read_lines("inputs/input6.txt")?;
+    let mut _res = 0;
+    for line in lines {
+        let line = line?;
+        for idx in 0..line.chars().count() + 1 {
+            if idx > 3 && !all_different_in_substr(&line[idx - 4..idx]) {
+                _res = idx;
+                break;
+            }
+        }
+    }
+    println!("Result is {}", _res);
+    Ok(())
+}
+
+fn question2() -> Result<()> {
+    let lines = read_lines("inputs/input6.txt")?;
+    let mut _res = 0;
+    for line in lines {
+        let line = line?;
+        for idx in 0..line.chars().count() + 1 {
+            if idx > 13 && !all_different_in_substr(&line[idx - 14..idx]) {
+                _res = idx;
+                break;
+            }
+        }
+    }
+    println!("Result is {}", _res);
+    Ok(())
+}
+
+fn all_different_in_substr(substr: &str) -> bool {
+    let mut res = false;
+    let mut tmp = substr.to_string();
+    for (index, char) in substr.chars().enumerate() {
+        tmp.remove(index).to_string();
+        if tmp.contains(char) {
+            res = true;
+            break;
+        }
+        tmp = substr.to_string();
+    }
+    res
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
